815f49c145d7070f550285c80ec6a2a946f94722
Mr. Will <mr.will.com@outlook.com>
2024-08-21 11:16:42 +0800

    Add `/c`

--- config.js
+++ config.js
@@ -2,7 +2,11 @@ const config = {
   redirects: [
     {
       from: '/messy-math',
-      to: 'https://blog.mrwillcom.com/2024/06/02/The-Messy-Math/',
+      to: 'https://blog.mrwillcom.com/2024/06/02/The-Messy-Math',
+    },
+    {
+      from: '/c',
+      to: 'https://github.com/MrWillCom/hexo-theme-cupertino',
     },
   ],
 }
