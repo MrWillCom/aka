4e5eaf997f2d199e5e6edea89556c91a82de3582
Mr. Will <mr.will.com@outlook.com>
2024-08-21 11:50:11 +0800

    Add `/` page

--- api/index.ts
+++ api/index.ts
@@ -9,7 +9,48 @@ export const config = {
 const app = new Hono().basePath('/')
 
 app.get('/', c => {
-  return c.json({ message: 'Hello Hono!' })
+  const ce = (tag: string, attr?: string | null, children?: string | null) =>
+    `<${tag} ${attr ?? ''}>${children ?? ''}</${tag}>`
+
+  let html = ''
+  html += ce('h1', null, 'aka')
+  html += ce('p', null, 'A tiny redirector for shortening links.')
+  html += ce('p', null, 'Redirects:')
+  html += ce(
+    'ul',
+    null,
+    userConfig.redirects
+      .map(r =>
+        ce(
+          'li',
+          null,
+          ce('a', `href="${r.from}"`, ce('pre', null, `${r.from} -> ${r.to}`)),
+        ),
+      )
+      .join(''),
+  )
+  html += ce(
+    'footer',
+    null,
+    [
+      ce('p', null, '© 2024 Mr. Will'),
+      ce(
+        'p',
+        null,
+        [
+          ce(
+            'a',
+            'href="https://github.com/MrWillCom/aka" target="_blank"',
+            'Source Code',
+          ),
+          '<br />',
+          'Powered by ',
+          ce('a', 'href="https://hono.dev/" target="_blank"', 'Hono'),
+        ].join(''),
+      ),
+    ].join(''),
+  )
+  return c.html(html)
 })
 
 userConfig.redirects.forEach(r => {
