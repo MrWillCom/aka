cb9431c91f4d091046d29367a1844a6c18dd10b6
Mr. Will <mr.will.com@outlook.com>
2025-09-28 17:41:01 +0800

    Add `/mhpr`

--- config.js
+++ config.js
@@ -1,6 +1,10 @@
 const config = {
   redirects: [
     {
+      from: '/mhpr',
+      to: 'https://blog.mrwillcom.com/2025/09/28/My-Home-Page-Reimagined/',
+    },
+    {
       from: '/ctw',
       to: 'https://github.com/MrWillCom/copy-this-way',
     },
